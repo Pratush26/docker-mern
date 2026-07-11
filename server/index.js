6c53086f05b64a4cdbd512cffdf8a117e6eefdce
Pratush Barua <pratushbarua26@gmail.com>
2026-07-11 20:16:16 +0600

    added frontend and backend initial setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+const allowedOrigins = process.env.FRONTENDS
+  ? process.env.FRONTENDS.split(",").map(s => s.trim())
+  : ["http://localhost:5173"];
+
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+app.use(express.json());
+
+app.get("/", async (req, res) => res.send("Server is getting!"))
+
+app.listen(port, () => console.log(`Server listening on Port - ${port}`))
